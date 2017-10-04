a22e8de0c856e58783ad680c5649d07d9fcb80c6
cristian <cristian@Cristian>
2017-10-04 15:27:35 -0300

    Modificaciones en PersonaBasicaValidator

--- src/main/java/presentacion/validators/PersonaBasicaValidator.java
+++ src/main/java/presentacion/validators/PersonaBasicaValidator.java
@@ -20,8 +20,23 @@ public class PersonaBasicaValidator implements Validator<PersonaBasica>{
 		if(hayCamposVacios(t)) {
 			msgShw.showErrorMessage("Hay Campos Vacios", "Error");
 			return false;
+		} else if(isNameValid(t)) {
+			msgShw.showErrorMessage("El nombre y el apellido solo pueden contener letras", "Error");
+			return false;
+		} else if(isEmailValid(t)) {
+			msgShw.showErrorMessage("La direccion de mail es invalida", "Error");
+			return false;
 		}
 		return true;
+	}
+	
+	private boolean isNameValid(PersonaBasica t) {		
+		return t.getNombre().matches(Regex.onlyLetters()) && 
+				t.getApellido().matches(Regex.onlyLetters());
+	}
+	
+	private boolean isEmailValid(PersonaBasica t) {		
+		return t.getEmail().matches(Regex.email());
 	}
 	
 	private boolean hayCamposVacios(PersonaBasica t) {
