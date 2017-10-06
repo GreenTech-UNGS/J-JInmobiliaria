aa34aa1f4adb4e2155973348b85810b3f8c36cf1
lucas <lucas.addisi@gmail.com>
2017-10-06 12:45:29 -0300

    Se agregó funcionalidad a la clase PropiedadComboBox

--- src/main/java/presentacion/combo/PropiedadComboBoxModel.java
+++ src/main/java/presentacion/combo/PropiedadComboBoxModel.java
@@ -7,16 +7,27 @@ import java.util.List;
 public class PropiedadComboBoxModel extends BaseComboBoxModel<Propiedad> {
     @Override
     public void actualize(List<Propiedad> list) {
-
+        list.forEach(p -> agregaElemento(p));
     }
 
     @Override
     public void agregaElemento(Propiedad element) {
-
+        String addedElement = element.getIdentificador().toString();
+        addElement(addedElement);
+        values.put(addedElement, element);
     }
 
     @Override
     public void setSelected(Propiedad toSelect) {
+        if(toSelect == null){
+            this.setSelectedItem(null);
+        }
+        else{
 
+            String nombre = toSelect.toString();
+
+            this.setSelectedItem(nombre);
+
+        }
     }
 }
