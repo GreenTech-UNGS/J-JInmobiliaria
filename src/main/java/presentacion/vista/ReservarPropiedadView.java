54b949f571db4e7402fd4e069eaa15dbda5c707f
lucasaddisi <lucas.addisi@gmail.com>
2017-10-06 12:00:26 -0300

    Se ha creado la ventana de Reserva de propiedades

--- src/main/java/presentacion/vista/ReservarPropiedadView.java
+++ src/main/java/presentacion/vista/ReservarPropiedadView.java
@@ -1,4 +1,62 @@
 package presentacion.vista;
 
-public class ReservarPropiedadView {
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+
+public class ReservarPropiedadView extends JDialog {
+	
+	private JPanel panel;
+	private JButton btnGuardar;
+	private JButton btnCancelar;
+	
+	JComboBox<String> comboCliente;
+	JComboBox<String> comboPropiedad;
+	
+	private ReservarPropiedadView(){
+		super();
+		
+		setTitle("Reservar propiedad");
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setModal(true);
+		setSize(new Dimension(350, 420));
+		setResizable(false);
+		setLocationRelativeTo(null);
+		
+		panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Cliente:");
+		lblNombre.setBounds(20, 83, 80, 20);
+		panel.add(lblNombre);
+		
+		JLabel lblMail = new JLabel("Propiedad:");
+		lblMail.setBounds(20, 145, 80, 20);
+		panel.add(lblMail);
+		
+		btnGuardar = new JButton("Guardar");
+		btnGuardar.setBounds(20, 330, 135, 42);
+		panel.add(btnGuardar);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(189, 330, 123, 42);
+		panel.add(btnCancelar);
+		
+		comboCliente = new JComboBox<String>();
+		comboCliente.setBounds(110, 83, 160, 20);
+		panel.add(comboCliente);
+		
+		comboPropiedad = new JComboBox<String>();
+		comboPropiedad.setBounds(110, 145, 160, 20);
+		panel.add(comboPropiedad);
+	}
 }
