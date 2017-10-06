c7ae804833669b1c4647cac59b9beb01d2b81711
lucas <lucas.addisi@gmail.com>
2017-10-06 12:39:25 -0300

    Se agregó la clase PropiedadComboBoxModel

---
+++ src/main/java/presentacion/combo/PropiedadComboBoxModel.java
@@ -0,0 +1,22 @@
+package presentacion.combo;
+
+import entities.Propiedad;
+
+import java.util.List;
+
+public class PropiedadComboBoxModel extends BaseComboBoxModel<Propiedad> {
+    @Override
+    public void actualize(List<Propiedad> list) {
+
+    }
+
+    @Override
+    public void agregaElemento(Propiedad element) {
+
+    }
+
+    @Override
+    public void setSelected(Propiedad toSelect) {
+
+    }
+}
--- src/main/java/presentacion/controller/ReservarPropiedadController.java
+++ src/main/java/presentacion/controller/ReservarPropiedadController.java
@@ -1,17 +1,32 @@
 package presentacion.controller;
 
 import com.google.inject.Inject;
+import entities.Cliente;
+import entities.Propiedad;
+import misc.Binder;
 import presentacion.vista.ReservarPropiedadView;
 
 public class ReservarPropiedadController {
     private ReservarPropiedadView view;
+
+    private Binder<Cliente> clienteBinder;
+    private Binder<Propiedad> propiedadBinder;
 
     @Inject
     public ReservarPropiedadController(ReservarPropiedadView view) {
         this.view = view;
 
         view.getBtnCancelar().addActionListener(e -> closeView());
+
+        this.clienteBinder = new Binder<>();
+        this.propiedadBinder = new Binder<>();
     }
+
+    private void fillComboCliente(){
+
+    }
+
+
 
     public void showView(){
         view.setVisible(true);
