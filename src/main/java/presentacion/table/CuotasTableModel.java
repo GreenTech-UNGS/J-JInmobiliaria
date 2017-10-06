c41e622e0c207678c801a76b7063e5bf285b87de
Cristian <Cristian@PC203>
2017-10-06 20:53:55 -0300

    Modelo de Tabla de Cuotas

---
+++ src/main/java/presentacion/table/CuotasTableModel.java
@@ -0,0 +1,32 @@
+package presentacion.table;
+
+import entities.CuotaAlquiler;
+
+@SuppressWarnings("serial")
+public class CuotasTableModel extends BaseTableModel<CuotaAlquiler> {
+	
+	public CuotasTableModel(){
+		
+	super.addColumn("DNI Inquilino", false, 100);
+	super.addColumn("ID Propiedad", false, 100);
+	super.addColumn("ID Contrato", false, 100);
+	super.addColumn("Valor", false, 100);
+	}
+
+	@Override
+	protected Object[] toRow(CuotaAlquiler t) {
+		
+		float temp = t.getMonto().getMonto();
+		String valor = t.getMonto().getMoneda().toString() + temp;
+		
+		Object[] fila = {
+				t.getContrato().getCliente().getPersona().getCredencial(),
+				t.getContrato().getPropiedad().getIdentificador(),
+				t.getContrato().getID(),
+				valor,
+		};
+
+		return fila;
+	}
+
+}
