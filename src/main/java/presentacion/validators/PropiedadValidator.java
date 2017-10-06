0cc7fdd0308a902786f7f5929fa2f7c576a68742
Cristian <Cristian@PC203>
2017-10-06 19:51:47 -0300

    Se modifico el validator de Propiedad

--- src/main/java/presentacion/validators/PropiedadValidator.java
+++ src/main/java/presentacion/validators/PropiedadValidator.java
@@ -34,10 +34,7 @@ public class PropiedadValidator implements Validator<Propiedad>{
 	public boolean isValid(Propiedad t) {
 
 		if(hayCamposVacios(t)){
-			return false;
-		}
-		
-		if(t.getPrecioTentativo().getMonto() <= 0){			
+			msgShw.showErrorMessage("Debe completar todos los campos obligatorios", "Error");
 			return false;
 		}
 		
@@ -50,14 +47,22 @@ public class PropiedadValidator implements Validator<Propiedad>{
 			msgShw.showErrorMessage("La inmobiliaria asociada tiene datos incorrectos", "Error");
 			return false;
 		}
+
+		if(t.getPrecioTentativo().getMonto() <= 0){		
+			msgShw.showErrorMessage("Ingrese un precio valido", "Error");
+			return false;
+		}
+		
 		return true;
 	}
 	
 	private boolean hayCamposVacios(Propiedad t) {
+		
 		return 
-				t.getIdentificador().equals("") ||
-				t.getCalle().equals("") ||
-				t.getAltura().equals("");
+				t.getIdentificador().equals("") || 
+				t.getCalle().equals("") || 
+				t.getAltura().equals("") || 
+				t.getLocalidad().getNombre() == null;
 	}
 	
 }
