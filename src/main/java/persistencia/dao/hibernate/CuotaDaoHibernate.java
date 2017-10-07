9abaebed012ea97c85520ce060647cbec56c9c24
Cristian <Cristian@PC203>
2017-10-06 21:27:45 -0300

    Metodo GetPendientes() en CuotaDao (A refactorizar)

--- src/main/java/persistencia/dao/hibernate/CuotaDaoHibernate.java
+++ src/main/java/persistencia/dao/hibernate/CuotaDaoHibernate.java
@@ -1,5 +1,6 @@
 package persistencia.dao.hibernate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -8,7 +9,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import entities.CuotaAlquiler;
-import entities.Localidad;
+import entities.EstadoCuota;
 import persistencia.conexion.Conexion;
 import persistencia.dao.iface.CuotaDao;
 
@@ -32,8 +33,26 @@ public class CuotaDaoHibernate extends DaoHibernate<CuotaAlquiler> implements Cu
 		return q.list();
 	}
 	
-	
+	@Override
+	public List<CuotaAlquiler> getPendientes() {
+		initTransaction();
+		
+		Criteria q = sesion.createCriteria(CuotaAlquiler.class);
+		
+		finishTransaction();
 
-	
+		//TODO: FILTRAR DENTRO DE LA QUERY
+		
+		List<CuotaAlquiler> toReturn = new ArrayList<CuotaAlquiler>();
+		for(Object cuota : q.list()){		
+			//TODO: ORDENAR ESTADOS POR FECHA
+			if(((CuotaAlquiler) cuota).getEstados().get(((CuotaAlquiler) cuota).getEstados().size()-1).equals(EstadoCuota.PENDIENTE)){
+				
+				toReturn.add((CuotaAlquiler) cuota);
+			}			
+		}
+		
+		return toReturn;
+	}
 	
 }
--- src/main/java/persistencia/dao/iface/CuotaDao.java
+++ src/main/java/persistencia/dao/iface/CuotaDao.java
@@ -1,7 +1,10 @@
 package persistencia.dao.iface;
 
+import java.util.List;
+
 import entities.CuotaAlquiler;
 
 public interface CuotaDao extends Dao<CuotaAlquiler>{
 
+	public List<CuotaAlquiler> getPendientes();
 }
