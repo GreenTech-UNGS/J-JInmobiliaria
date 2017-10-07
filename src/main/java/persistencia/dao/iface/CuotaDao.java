50b0fd65651e41ea60898d7a39fc7b4c80c68b3a
Leandro Funes <funes.s.leandro@gmail.com>
2017-10-07 18:55:10 -0300

    Se muestran las cuotas de alquiler en la pestaña de pagos

--- src/main/java/persistencia/dao/iface/CuotaDao.java
+++ src/main/java/persistencia/dao/iface/CuotaDao.java
@@ -2,12 +2,14 @@ package persistencia.dao.iface;
 
 import java.util.List;
 
+import org.joda.time.YearMonth;
+
 import entities.CuotaAlquiler;
 import entities.InteresPunitorioCuota;
 
 public interface CuotaDao extends Dao<CuotaAlquiler>{
 
-	public List<CuotaAlquiler> getPendientes();
+	public List<CuotaAlquiler> getAllOf(YearMonth anioMes);
 	List<CuotaAlquiler> getAllOfThisMonth();
 	InteresPunitorioCuota getInteresOf(CuotaAlquiler c);
 	
