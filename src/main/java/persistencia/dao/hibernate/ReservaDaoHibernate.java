1856e879a86e94346411a33d31d94f60a3d29952
lucas <lucas.addisi@gmail.com>
2017-10-08 11:10:33 -0300

    Se ha agregado implementado la clase ReservaDaoHibernate

--- src/main/java/persistencia/dao/hibernate/ReservaDaoHibernate.java
+++ src/main/java/persistencia/dao/hibernate/ReservaDaoHibernate.java
@@ -1,4 +1,19 @@
 package persistencia.dao.hibernate;
 
-public class ReservaDaoHibernate {
+import entities.Reserva;
+import persistencia.conexion.Conexion;
+import persistencia.dao.iface.ReservaDAO;
+
+import java.util.List;
+
+public class ReservaDaoHibernate extends DaoHibernate<Reserva> implements ReservaDAO {
+
+    protected ReservaDaoHibernate(Conexion conexion) {
+        super(conexion);
+    }
+
+    @Override
+    public List<Reserva> getAll() {
+        throw new RuntimeException("No implementado aún");
+    }
 }
