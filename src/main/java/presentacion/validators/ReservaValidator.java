3a7776df2fa3aa4a8c4570ef2372004c4e74994a
Cristian <Cristian@CristianMC>
2017-10-09 02:15:15 -0300

    Se agrega Reserva Validator

--- src/main/java/presentacion/validators/ReservaValidator.java
+++ src/main/java/presentacion/validators/ReservaValidator.java
@@ -2,18 +2,32 @@ package presentacion.validators;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+
+import entities.EstadoProp;
 import entities.Reserva;
+import model.PropiedadService;
 
 @Singleton
 public class ReservaValidator implements Validator<Reserva>{
 
+	PropiedadService propiedadService;
+	MessageShow msgShw;
+	
     @Inject
-    public ReservaValidator() {
-
+    public ReservaValidator(PropiedadService propiedadService,
+    		MessageShow msgShw) {
+    	this.propiedadService = propiedadService;
+    	this.msgShw = msgShw;
     }
 
     @Override
     public boolean isValid(Reserva reserva) {
-        return false;
+       
+    	if(propiedadService.getCurrentEstado(reserva.getPropiedad()).equals(EstadoProp.DISPONIBLE) == false){
+    		msgShw.showErrorMessage("La propiedad seleccionada no esta disponible", "Error");
+			return false;
+    	}
+    		
+    	return false;
     }
 }
