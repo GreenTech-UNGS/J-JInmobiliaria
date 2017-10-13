270b2c01ef73919023f3042b8280764ffb30737c
Cristian <Cristian@CristianMC>
2017-10-12 21:48:55 -0300

    Modificaciones en PropietarioValidator

--- src/main/java/presentacion/validators/PropietarioValidator.java
+++ src/main/java/presentacion/validators/PropietarioValidator.java
@@ -4,20 +4,35 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import entities.Propietario;
+import model.PropietarioService;
 
 @Singleton
 public class PropietarioValidator implements Validator<Propietario>{
 
 	PersonaValidator pValidator;
+	PropietarioService propietarioService;
+	MessageShow msgShw;
 	
 	@Inject
-	private PropietarioValidator(PersonaValidator pValidator, MessageShow msgShw) {
+	private PropietarioValidator(PersonaValidator pValidator, 
+			PropietarioService propietarioService,
+			MessageShow msgShw) {
 		this.pValidator = pValidator;
+		this.propietarioService = propietarioService;
+		this.msgShw = msgShw;
 	}
 	
 	@Override
 	public boolean isValid(Propietario t) {
-		return t != null && pValidator.isValid(t.getPersona());
+		
+
+		if(propietarioService.existePropietarioCon(t.getPersona())) {
+			msgShw.showErrorMessage("El propietario ya esta agregado", "Error");
+			return false;
+		}
+		else{
+			return t != null && pValidator.isValid(t.getPersona());
+		}
 	}
 	
 }
