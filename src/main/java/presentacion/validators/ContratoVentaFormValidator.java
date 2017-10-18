f58607eff60a4e4cfc7583d0dfcd5f70cd9b21fd
Cristian <Cristian@CristianMC>
2017-10-18 18:55:47 -0300

    Implementacion de Validator para Contrato Venta

--- src/main/java/presentacion/validators/ContratoVentaFormValidator.java
+++ src/main/java/presentacion/validators/ContratoVentaFormValidator.java
@@ -17,15 +17,75 @@ public class ContratoVentaFormValidator implements ValidatorNew{
 	
 	@Override
 	public boolean isValid() {
-		return true;
+		return isIdValid() 
+				&& isPropiedadValid()
+				&& isClienteValid()
+				&& isGarantiaValid()
+				&& isPrecioValid()
+				&& isGarantiaValid();
 	}
 
 	@Override
 	public String getErrorMessage() {
 		String toRet = "Errores en los siguientes campos";
+		if(!isIdValid())
+			toRet += "\n- El identificador no es valido";
+		if(!isPropiedadValid())
+			toRet += "\n- No ha seleccionado una propiedad valida";
+		if(!isClienteValid())
+			toRet += "\n- No ha seleccionado un cliente valido";
+		if(!isGarantiaValid())
+			toRet += "\n- La garantia no es valido";
+		if(!isPrecioValid())
+			toRet += "\n- El precio no es valido";
+		if(!isGastosAdmValid())
+			toRet += "\n- El porcentaje de gastos administrativos debe ser mayor a cero";	
 		
 		return toRet;
 		
 	}
-
+		
+	private boolean isIdValid(){
+		String id = view.getTfIdContrato().getText();
+		
+		if(id == null || id.equals(""))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isPropiedadValid(){
+		String propiedad = view.getTfPropiedad().getText();
+		
+		if(propiedad == null || propiedad.equals(""))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isClienteValid(){
+		String cliente = view.getTfCliente().getText();
+		
+		if(cliente == null || cliente.equals(""))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isGarantiaValid(){
+		String garantia = view.getTfGarantia().getText();
+		
+		if(garantia == null || garantia.equals(""))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isPrecioValid(){
+		return view.getTfPrecio().getText().matches("([0-9]*[\\.])?[0-9]+");
+	}	
+		
+	private boolean isGastosAdmValid(){
+		return ((Integer) view.getSpinnerPorcentaje().getValue() <= 0);
+	}
 }
