5f681b8fa9c239a189a0cbf336006d5c68d167bd
Cristian <Cristian@CristianMC>
2017-10-23 02:33:07 -0300

    Se implementa InmobiliariaValidator

--- src/main/java/presentacion/validators/InmobiliariaFormValidator.java
+++ src/main/java/presentacion/validators/InmobiliariaFormValidator.java
@@ -3,6 +3,7 @@ package presentacion.validators;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import entities.Persona.TipoCredencial;
 import presentacion.vista.InmobiliariaForm;
 
 @Singleton
@@ -18,14 +19,89 @@ public class InmobiliariaFormValidator implements ValidatorNew {
 
 	@Override
 	public boolean isValid() {
-		// TODO Auto-generated method stub
-		return true;
+		return isCredencialValid() 
+				&& isEmailValid()
+				&& isNombreValid()
+				&& isCalleValid()
+				&& isAlturaValid()
+				&& isLocalidadValid();
 	}
 
 	@Override
 	public String getErrorMessage() {
 		String toRet = "Errores en los siguientes campos";
+		if(!isCredencialValid())
+			toRet += "\n- El CUIT no es valido";
+		if(!isEmailValid())
+			toRet += "\n- El email no es valido";
+		if(!isNombreValid())
+			toRet += "\n- El nombre no es valido";
+		if(!isCalleValid())
+			toRet += "\n- La calle no es valida";
+		if(!isAlturaValid())
+			toRet += "\n- La altura no es valida";
+		if(!isLocalidadValid())
+			toRet += "\n- No ha seleccionado ninguna localidad";
+		
 		return toRet;
+	}
+	
+	private boolean isCredencialValid(){
+		String credencial = view.getTfCuit().getText();
+
+		if(credencial == null || (!credencial.matches(Regex.CUIT()) && !credencial.matches(Regex.onlyNumbersCUIT())))
+			return false;
+			
+		return true;
+	}
+	
+	private boolean isEmailValid(){
+		String email = view.getTfEmail().getText();
+		
+		if(email == null || !email.matches(Regex.email()))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isNombreValid(){
+		String nombre = view.getTfNombre().getText();
+		
+		if(nombre == null || nombre.equals("") || nombre.matches("\\b*"))
+			return false;
+		if( ! nombre.matches(Regex.onlyLetters()))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isCalleValid(){
+		String calle = view.getTfCalle().getText();
+		
+		if(calle == null || calle.equals("") || calle.matches("\\b*"))
+			return false;
+
+		return true;
+	}
+	
+	private boolean isAlturaValid(){
+		String localidad = view.getTfAltura().getText();
+		
+		if(localidad == null || localidad.equals("") || localidad.matches("\\b*"))
+			return false;
+		if( ! localidad.matches(Regex.onlyNumbers()))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isLocalidadValid(){
+		String localidad = (String) view.getCbLocalidad().getSelectedItem();
+		
+		if(localidad == null || localidad.equals("") || localidad.matches("\\b*"))
+			return false;
+		
+		return true;
 	}
 
 }
