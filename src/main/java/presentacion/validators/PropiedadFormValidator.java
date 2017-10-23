e68e5a2ba7bec9569598392e9a06c457559b4d8b
Cristian <Cristian@CristianMC>
2017-10-23 02:32:56 -0300

    Se implementa PropiedadValidator

--- src/main/java/presentacion/validators/PropiedadFormValidator.java
+++ src/main/java/presentacion/validators/PropiedadFormValidator.java
@@ -18,22 +18,70 @@ public class PropiedadFormValidator implements ValidatorNew{
 	
 	@Override
 	public boolean isValid() {
-		// TODO Auto-generated method stub
-		return isPrecioValid();
-	}
+		return isIdentificadorValid() 
+				&& isCalleValid()
+				&& isAlturaValid()
+				&& isLocalidadValid()
+				&& isPrecioValid();
+	}	
 
 	@Override
 	public String getErrorMessage() {
 		String toRet = "Error en los siguientes campos: ";
-		
+		if(!isIdentificadorValid())
+			toRet += "\n- El codigo no es valido";
+		if(!isCalleValid())
+			toRet += "\n- La calle no es valida";
+		if(!isAlturaValid())
+			toRet += "\n- La altura no es valida";
+		if(!isLocalidadValid())
+			toRet += "\n- No ha seleccionado ninguna localidad";
 		if(!isPrecioValid())
 			toRet += "\n- El precio no es valido";
 		
 		return toRet;
 	}
 	
+	private boolean isIdentificadorValid(){
+		String identificador = view.getTfIdentificador().getText();
+		
+		if(identificador == null || identificador.equals("") || identificador.matches("\\b*"))
+			return false;
+		
+		return true;
+	}
+	
 	private boolean isPrecioValid(){
 		return view.getTfPrecio().getText().matches("([0-9]*[\\.])?[0-9]+");
+	}
+	
+	private boolean isCalleValid(){
+		String calle = view.getTfCalle().getText();
+		
+		if(calle == null || calle.equals("") || calle.matches("\\b*"))
+			return false;
+
+		return true;
+	}
+	
+	private boolean isAlturaValid(){
+		String localidad = view.getTfAltura().getText();
+		
+		if(localidad == null || localidad.equals("") || localidad.matches("\\b*"))
+			return false;
+		if( ! localidad.matches(Regex.onlyNumbers()))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isLocalidadValid(){
+		String localidad = (String) view.getComboLocalidad().getSelectedItem();
+		
+		if(localidad == null || localidad.equals("") || localidad.matches("\\b*"))
+			return false;
+		
+		return true;
 	}
 
 }
