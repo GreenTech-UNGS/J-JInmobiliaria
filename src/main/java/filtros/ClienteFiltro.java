ed9e1e5dfb703e7221ef09db4c269bdc4de7f971
Cristian <Cristian@CristianMC>
2017-10-23 01:18:30 -0300

    Se crea ClienteFiltro

---
+++ src/main/java/filtros/ClienteFiltro.java
@@ -0,0 +1,47 @@
+package filtros;
+
+import entities.Persona.TipoCredencial;
+
+public class ClienteFiltro {
+	
+	String nombre;
+	String apellido;
+	String credencial;
+	TipoCredencial tipoCredencial;
+	
+	public ClienteFiltro() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public String getCredencial() {
+		return credencial;
+	}
+
+	public TipoCredencial getTipoCredencial() {
+		return tipoCredencial;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setCredencial(String credencial) {
+		this.credencial = credencial;
+	}
+
+	public void setTipoCredencial(TipoCredencial tipoCredencial) {
+		this.tipoCredencial = tipoCredencial;
+	}
+	
+}
