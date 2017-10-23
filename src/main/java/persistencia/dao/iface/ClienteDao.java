a30cfb949021d5b24d4c0c33959904399d68a428
Cristian <Cristian@CristianMC>
2017-10-23 01:17:47 -0300

    Se agrega el metodo getAllByFiltro

--- src/main/java/persistencia/dao/iface/ClienteDao.java
+++ src/main/java/persistencia/dao/iface/ClienteDao.java
@@ -1,12 +1,15 @@
 package persistencia.dao.iface;
 
+import java.util.List;
+
 import entities.Cliente;
 import entities.Persona;
+import filtros.ClienteFiltro;
 
 public interface ClienteDao extends Dao<Cliente>{
 
 	boolean existeClienteCon(Persona t);
-	
+	public List<Cliente> getAllByFiltro(ClienteFiltro filtro);
 	public void actualizeCliente(Cliente toActualize);
 
 }
