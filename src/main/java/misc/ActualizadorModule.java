a146727eda63c0a194b6594c7d9e04c12437af89
leafunes <funes.s.leandro@gmail.com>
2017-10-24 15:57:54 -0300

    Se reparo el actualizador de cuotas

--- src/main/java/misc/ActualizadorModule.java
+++ src/main/java/misc/ActualizadorModule.java
@@ -6,9 +6,11 @@ import com.google.inject.Singleton;
 import persistencia.dao.hibernate.ContratoDaoHibernate;
 import persistencia.dao.hibernate.CuotaDaoHibernate;
 import persistencia.dao.hibernate.PropiedadDaoHibernate;
+import persistencia.dao.hibernate.ReservaDaoHibernate;
 import persistencia.dao.iface.ContratoDao;
 import persistencia.dao.iface.CuotaDao;
 import persistencia.dao.iface.PropiedadDao;
+import persistencia.dao.iface.ReservaDAO;
 
 public class ActualizadorModule implements Module{
 
@@ -18,7 +20,7 @@ public class ActualizadorModule implements Module{
 		b.bind(CuotaDao.class).to(CuotaDaoHibernate.class).in(Singleton.class);
 		b.bind(ContratoDao.class).to(ContratoDaoHibernate.class).in(Singleton.class);
 		b.bind(PropiedadDao.class).to(PropiedadDaoHibernate.class).in(Singleton.class);
-		
+		b.bind(ReservaDAO.class).to(ReservaDaoHibernate.class).in(Singleton.class);
 		
 	}
 
