9f56c92429a77bd8395bf24781e65ecab000c2cd
Leandro Funes <funes.s.leandro@gmail.com>
2017-10-26 21:03:11 -0300

    Se creó el login view

---
+++ src/main/java/presentacion/main/vista/LoginView.java
@@ -0,0 +1,15 @@
+package presentacion.main.vista;
+
+import javax.swing.JFrame;
+
+public class LoginView {
+
+	private JFrame loginFrame;
+	
+	public LoginView() {
+		
+		loginFrame = new JFrame("Login");
+		
+	}
+	
+}
