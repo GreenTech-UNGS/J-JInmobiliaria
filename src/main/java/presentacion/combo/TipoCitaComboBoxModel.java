7ee2fc7f6cac4ffbf06378dc9a9ff73f2686008e
Leandro Funes <funes.s.leandro@gmail.com>
2017-10-29 23:56:57 -0300

    Se creo el combo model de tipo de cita

---
+++ src/main/java/presentacion/combo/TipoCitaComboBoxModel.java
@@ -0,0 +1,39 @@
+package presentacion.combo;
+
+import java.util.List;
+
+import entities.Provincia;
+import entities.TipoCita;
+
+public class TipoCitaComboBoxModel extends BaseComboBoxModel<TipoCita>{
+
+
+	@Override
+	public void actualize(List<TipoCita> list) {
+		
+		list.forEach(e -> agregaElemento(e));
+		
+	}
+
+	@Override
+	public void setSelected(TipoCita toSelect) {
+		if(toSelect == null){
+			this.setSelectedItem(null);
+		}
+		else{
+			
+		String nombre = toSelect.toString();
+		
+		this.setSelectedItem(nombre);
+		
+		}
+	}
+
+	@Override
+	public void agregaElemento(TipoCita element) {
+		this.addElement(element.toString().replaceAll("_", " "));
+		values.put(element.toString().replaceAll("_", " "), element);
+		
+	}
+
+}
