94bacffaf9c18988f4b27ea1b490842c3a490017
Cristian <Cristian@CristianMC>
2017-11-06 02:06:12 -0300

    Se agrega Mapper de Cartel

---
+++ J-JInmobiliaria-master/src/main/java/presentacion/mappers/CartelMapper.java
@@ -0,0 +1,53 @@
+package presentacion.mappers;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import entities.Cartel;
+import presentacion.vista.CartelForm;
+
+@Singleton
+public class CartelMapper implements Mapper<Cartel>{
+
+	private CartelForm view;
+	
+	@Inject
+	private CartelMapper(CartelForm view) {
+		this.view = view;
+	}
+	
+	@Override
+	public void fillBean(Cartel t) {
+		
+		int alto = (int) view.getSpinnerAlto().getValue();
+		int ancho = (int) view.getSpinnerAncho().getValue();
+		float monto = (float) view.getSpinnerMonto().getValue();
+		String identificador = view.getTextIdentificador().getText();
+		String descripcion = view.getTextDescripcion().getText();
+		
+		t.setAncho(ancho);
+		t.setAlto(alto);
+		t.setMonto(monto);
+		t.setIdentificador(identificador);
+		t.setDescripcion(descripcion);
+		
+	}
+
+	@Override
+	public void fillFields(Cartel t) {
+		
+		int alto = t.getAlto() == 0? 1 : t.getAlto();
+		int ancho = t.getAncho() == 0? 1 : t.getAncho();
+		float monto = t.getMonto() == 0? 1 : t.getMonto();
+		String identificador = t.getIdentificador();
+		String descripcion = t.getDescripcion();
+		
+		view.getSpinnerAncho().setValue(ancho);
+		view.getSpinnerAlto().setValue(alto);
+		view.getSpinnerMonto().setValue(monto);
+		view.getTextIdentificador().setText(identificador);
+		view.getTextDescripcion().setText(descripcion);
+		
+	}
+
+}
