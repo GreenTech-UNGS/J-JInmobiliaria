5339a3f99a563c7f44d1f4e48e53833ad0ba9633
Cristian <Cristian@CristianMC>
2017-11-06 04:29:12 -0300

    Se agrega Service de Cartel

---
+++ J-JInmobiliaria-master/src/main/java/model/CartelService.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+
+import entities.Cartel;
+import persistencia.dao.iface.CartelDao;
+import persistencia.dao.iface.PersonaDao;
+
+public class CartelService {
+	
+	CartelDao cartelDao;
+	PersonaDao personaDao;
+	
+	@Inject
+	private CartelService(CartelDao cartelDao) {
+		this.cartelDao = cartelDao;
+	}
+	
+	public Cartel getEmptyCartel() {
+		
+		Cartel toRet = new Cartel();		
+		return toRet;
+	}
+		
+	public void saveCartel(Cartel toSave) throws LogicaNegocioException {
+		
+		if(existeCartelCon(toSave.getIdentificador()))
+			throw new LogicaNegocioException("Ya existe un cartel con el mismo identificador");
+	
+		cartelDao.save(toSave);
+	}
+	
+	private boolean existeCartelCon(String identificador) {
+		
+		return cartelDao.existeCartelCon(identificador);
+	}
+
+	public List<Cartel> getAll(){
+		return cartelDao.getAll();
+	}
+	
+}
