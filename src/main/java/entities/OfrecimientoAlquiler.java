a3a22c0e36eddd9ad2283bc100d42bd3dd68e328
Leandro Funes <funes.s.leandro@gmail.com>
2017-11-13 21:49:29 -0300

    Se crearon las entities para los ofrecimientos

---
+++ src/main/java/entities/OfrecimientoAlquiler.java
@@ -0,0 +1,109 @@
+package entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ofrecimientosAlquiler")
+public class OfrecimientoAlquiler {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int ID;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Precio precio;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Precio otrosGastos;
+	
+	private boolean isHabilitada;
+	private int cantidadMeses;
+	private int intervaloActualizacion;
+	private float procentajeActualizacion;
+	private boolean isAcumulativo;
+	
+	
+	public OfrecimientoAlquiler() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Precio getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(Precio precio) {
+		this.precio = precio;
+	}
+
+
+	public Precio getOtrosGastos() {
+		return otrosGastos;
+	}
+
+
+	public void setOtrosGastos(Precio otrosGastos) {
+		this.otrosGastos = otrosGastos;
+	}
+
+
+	public int getCantidadMeses() {
+		return cantidadMeses;
+	}
+
+
+	public void setCantidadMeses(int cantidadMeses) {
+		this.cantidadMeses = cantidadMeses;
+	}
+
+
+	public int getIntervaloActualizacion() {
+		return intervaloActualizacion;
+	}
+
+
+	public void setIntervaloActualizacion(int intervaloActualizacion) {
+		this.intervaloActualizacion = intervaloActualizacion;
+	}
+
+
+	public float getProcentajeActualizacion() {
+		return procentajeActualizacion;
+	}
+
+
+	public void setProcentajeActualizacion(float procentajeActualizacion) {
+		this.procentajeActualizacion = procentajeActualizacion;
+	}
+
+
+	public boolean isAcumulativo() {
+		return isAcumulativo;
+	}
+
+
+	public void setAcumulativo(boolean isAcumulativo) {
+		this.isAcumulativo = isAcumulativo;
+	}
+
+
+	public int getID() {
+		return ID;
+	}
+
+
+	public boolean isHabilitada() {
+		return isHabilitada;
+	}
+
+
+	public void setHabilitada(boolean isHabilitada) {
+		this.isHabilitada = isHabilitada;
+	}
+
+}
---
+++ src/main/java/entities/OfrecimientoVenta.java
@@ -0,0 +1,68 @@
+package entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ofrecimientosVenta")
+public class OfrecimientoVenta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int ID;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Precio precio;
+	@ManyToOne(cascade = CascadeType.ALL)
+	
+	private boolean isHabilitada;
+	private float comisionComprador;
+	private float comisionVendedor;
+	
+	public OfrecimientoVenta() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Precio getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Precio precio) {
+		this.precio = precio;
+	}
+
+	public boolean isHabilitada() {
+		return isHabilitada;
+	}
+
+	public void setHabilitada(boolean isHabilitada) {
+		this.isHabilitada = isHabilitada;
+	}
+
+	public float getComisionComprador() {
+		return comisionComprador;
+	}
+
+	public void setComisionComprador(float comisionComprador) {
+		this.comisionComprador = comisionComprador;
+	}
+
+	public float getComisionVendedor() {
+		return comisionVendedor;
+	}
+
+	public void setComisionVendedor(float comisionVendedor) {
+		this.comisionVendedor = comisionVendedor;
+	}
+
+	public int getID() {
+		return ID;
+	}
+	
+	
+	
+}
