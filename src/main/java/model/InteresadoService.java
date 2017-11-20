d3178d1d6e008ea8116ec68196f8b29f65df790f
CristianMC94 <Cristian.mcg.94@gmail.com>
2017-11-20 03:31:26 -0300

    getAllByPropiedad y matches en InteresadoService

--- src/main/java/model/InteresadoService.java
+++ src/main/java/model/InteresadoService.java
@@ -1,13 +1,17 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import entities.EstadoProp;
 import entities.Interesado;
 import entities.Persona;
 import entities.Preferencia;
+import entities.Propiedad;
+import entities.TipoOfrecimiento;
 import filtros.InteresadoFiltro;
 import persistencia.dao.iface.InteresadoDao;
 import persistencia.dao.iface.PersonaDao;
@@ -56,7 +60,50 @@ public class InteresadoService {
 
 	public List<Interesado> getAllByFiltro(InteresadoFiltro filtro){
 	
-	return interesadoDao.getAllByFiltro(filtro);
+		return interesadoDao.getAllByFiltro(filtro);
 	
-}
+	}
+	
+	public List<Interesado> getAllByPropiedad(Propiedad propiedad){
+		List<Interesado> toRet = new ArrayList<Interesado>();
+		for(Interesado i : interesadoDao.getAll()){
+			if(this.matches(i.getPreferencia(), propiedad)){
+				toRet.add(i);
+			}
+		}				
+		return toRet;
+	}
+	
+	public boolean matches(Preferencia t, Propiedad p) {
+		
+		if(t.getTipoOfrecimiento().equals(TipoOfrecimiento.Alquiler) && p.getOfrecimientoAlquiler().isHabilitada() == false)
+			return false;
+		
+		if(t.getTipoOfrecimiento().equals(TipoOfrecimiento.Venta) && p.getOfrecimientoVenta().isHabilitada() == false)
+			return false;
+		
+		if( ! (p.getOfrecimientoVenta().getPrecio().getMoneda().equals(t.getMoneda())) )
+			return false;
+		
+		if(p.getOfrecimientoVenta().getPrecio().getMonto() < t.getPrecioDesde())
+			return false;
+		
+		if(p.getOfrecimientoVenta().getPrecio().getMonto() > t.getPrecioDesde())
+			return false;
+		
+		if(p.getOtrosDatos().getCantidadAmbientes() < t.getCantidadAmbientesDesde())
+			return false;
+		
+		if(p.getOtrosDatos().getCantidadAmbientes() > t.getCantidadAmbientesHasta())
+			return false;
+				
+		if(p.getOtrosDatos().getMetrosCuadradosLote() < t.getMetrosCuadradosDesde())
+			return false;
+		
+		if(p.getOtrosDatos().getMetrosCuadradosLote() > t.getMetrosCuadradosHasta())
+			return false;
+		
+		
+		return true;
+	}
 }
