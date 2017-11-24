0a976b6052635fa6e67ab5ccc4ea4f85d2273db0
CristianMC94 <Cristian.mcg.94@gmail.com>
2017-11-24 17:04:06 -0300

    Se amplia el mapper

--- src/main/java/presentacion/mappers/InteresadoFormMapper.java
+++ src/main/java/presentacion/mappers/InteresadoFormMapper.java
@@ -32,12 +32,15 @@ public class InteresadoFormMapper implements Mapper<Interesado>{
 		String metrosCuadradosHasta = view.getTfHastaMetros().getText();
 		String ambientesDesde = view.getTfDesdeAmbientes().getText();
 		String ambientesHasta = view.getTfHastaAmbientes().getText();
-		String precioDesde = view.getTfDesdePrecio().getText();
-		String precioHasta = view.getTfHastaPrecio().getText();
+		String precioDesdeVenta = view.getTfDesdePrecioVenta().getText();
+		String precioHastaVenta = view.getTfHastaPrecioVenta().getText();
+		String precioDesdeAlquiler = view.getTfDesdePrecioAlquiler().getText();
+		String precioHastaAlquiler = view.getTfHastaPrecioAlquiler().getText();
 		
 		TipoCredencial tipoCred = TipoCredencial.valueOf((String) view.getCbCredencial().getSelectedItem());
 		TipoOfrecimiento tipoOfrec = view.getComboModelOfrecimiento().getSelected();
-		Moneda moneda = view.getComboModelMoneda().getSelected();
+		Moneda monedaVenta = view.getComboModelMonedaVenta().getSelected();
+		Moneda monedaAlquiler = view.getComboModelMonedaAlquiler().getSelected();
 
 		Localidad localidad = view.getComboModelLocalidad().getSelected();
 		Provincia provincia = view.getComboModelProvincia().getSelected();
@@ -51,13 +54,16 @@ public class InteresadoFormMapper implements Mapper<Interesado>{
 		t.getPreferencia().setMetrosCuadradosHasta(Integer.parseInt(metrosCuadradosHasta));
 		t.getPreferencia().setCantidadAmbientesDesde(Integer.parseInt(ambientesDesde));
 		t.getPreferencia().setCantidadAmbientesHasta(Integer.parseInt(ambientesHasta));
-		t.getPreferencia().setPrecioDesde(Integer.parseInt(precioDesde));
-		t.getPreferencia().setPrecioHasta(Integer.parseInt(precioHasta));
+		t.getPreferencia().setPrecioVentaDesde(Integer.parseInt(precioDesdeVenta));
+		t.getPreferencia().setPrecioVentaHasta(Integer.parseInt(precioHastaVenta));
+		t.getPreferencia().setPrecioAlquilerDesde(Integer.parseInt(precioDesdeAlquiler));
+		t.getPreferencia().setPrecioAlquilerHasta(Integer.parseInt(precioHastaAlquiler));
 
 		
 		t.getPersona().setTipoCred(tipoCred);
 		t.getPreferencia().setTipoOfrecimiento(tipoOfrec);	
-		t.getPreferencia().setMoneda(moneda);
+		t.getPreferencia().setMonedaVenta(monedaVenta);
+		t.getPreferencia().setMonedaAlquiler(monedaAlquiler);
 		t.getPreferencia().setLocalidad(localidad);
 		t.setFechaAlta(DateTime.now());
 //		t.getPreferencia().getLocalidad().setProvincia(provincia);
@@ -76,12 +82,15 @@ public class InteresadoFormMapper implements Mapper<Interesado>{
 		String metrosCuadradosHasta = String.valueOf(t.getPreferencia().getMetrosCuadradosHasta());
 		String ambientesDesde = String.valueOf(t.getPreferencia().getCantidadAmbientesDesde());
 		String ambientesHasta = String.valueOf(t.getPreferencia().getCantidadAmbientesHasta());
-		String precioDesde = String.valueOf(t.getPreferencia().getPrecioDesde());
-		String precioHasta = String.valueOf(t.getPreferencia().getPrecioHasta());
+		String precioDesdeVenta = String.valueOf(t.getPreferencia().getPrecioVentaDesde());
+		String precioHastaVenta = String.valueOf(t.getPreferencia().getPrecioVentaHasta());
+		String precioDesdeAlquiler = String.valueOf(t.getPreferencia().getPrecioAlquilerDesde());
+		String precioHastaAlquiler = String.valueOf(t.getPreferencia().getPrecioAlquilerHasta());
 		
 		String tipoCred = String.valueOf(t.getPersona().getTipoCred());	
 		TipoOfrecimiento tipoOfrec = t.getPreferencia().getTipoOfrecimiento();	
-		Moneda moneda = t.getPreferencia().getMoneda();
+		Moneda monedaVenta = t.getPreferencia().getMonedaVenta();
+		Moneda monedaAlquiler = t.getPreferencia().getMonedaAlquiler();
 		Localidad localidad = t.getPreferencia().getLocalidad();
 		
 		view.getTfNombre().setText(nombre);
@@ -92,12 +101,15 @@ public class InteresadoFormMapper implements Mapper<Interesado>{
 		view.getTfHastaMetros().setText(metrosCuadradosHasta);
 		view.getTfDesdeAmbientes().setText(ambientesDesde);
 		view.getTfHastaAmbientes().setText(ambientesHasta);
-		view.getTfDesdePrecio().setText(precioDesde);
-		view.getTfHastaPrecio().setText(precioHasta);
+		view.getTfDesdePrecioVenta().setText(precioDesdeVenta);
+		view.getTfHastaPrecioVenta().setText(precioHastaVenta);
+		view.getTfDesdePrecioAlquiler().setText(precioDesdeAlquiler);
+		view.getTfHastaPrecioAlquiler().setText(precioHastaAlquiler);
 		
 		view.getCbCredencial().setSelectedItem(tipoCred);
 		view.getCbTipoOfrec().setSelectedItem(String.valueOf(tipoOfrec));
-		view.getCbMoneda().setSelectedItem(String.valueOf(moneda));
+		view.getCbMonedaVenta().setSelectedItem(String.valueOf(monedaVenta));
+		view.getCbMonedaAlquiler().setSelectedItem(String.valueOf(monedaAlquiler));
 		view.getCbLocalidad().setSelectedItem(String.valueOf(localidad));
 //		if(localidad != null){
 //			view.getCbProvincia().setSelectedItem(String.valueOf(localidad.getProvincia()));
