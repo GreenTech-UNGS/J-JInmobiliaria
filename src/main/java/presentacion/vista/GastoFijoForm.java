ac3c13b663875bc1042f474be57b07825da4db19
CristianMC94 <Cristian.mcg.94@gmail.com>
2017-11-28 11:26:42 -0300

    Se agrega vencimiento al Form de GastoFijo

--- src/main/java/presentacion/vista/GastoFijoForm.java
+++ src/main/java/presentacion/vista/GastoFijoForm.java
@@ -17,12 +17,15 @@ import com.google.inject.Singleton;
 
 import javax.swing.SwingConstants;
 
+import org.joda.time.DateTime;
+
 @Singleton
 public class GastoFijoForm extends JDialog{
 	private JTextField textNombre;
 	private JSpinner spinnerMonto;
 	private JTextArea textDescripcion;
 	private JButton btnAceptar;
+	private JTextField textVencimiento;
 	
 	@Inject
 	private GastoFijoForm() {
@@ -30,36 +33,36 @@ public class GastoFijoForm extends JDialog{
 		getContentPane().setLayout(null);
 		setModal(true);
 		setLocationRelativeTo(null);
-		setSize(new Dimension(425, 240));
+		setSize(new Dimension(425, 290));
 		
 		JLabel lblNombre = new JLabel("Gasto:");
 		lblNombre.setBounds(30, 30, 50, 20);
 		getContentPane().add(lblNombre);	
 
 		JLabel lblMonto = new JLabel("Monto:");
-		lblMonto.setBounds(250, 30, 50, 20);
+		lblMonto.setBounds(250, 70, 50, 20);
 		getContentPane().add(lblMonto);
 		
 		JLabel lblDescripcion = new JLabel("Descripcion:");
-		lblDescripcion.setBounds(30, 80, 80, 20);
+		lblDescripcion.setBounds(30, 110, 80, 20);
 		getContentPane().add(lblDescripcion);
 		
 		textNombre = new JTextField();
-		textNombre.setBounds(80, 30, 150, 20);
+		textNombre.setBounds(110, 30, 240, 20);
 		getContentPane().add(textNombre);
 		textNombre.setColumns(10);
 		
 		spinnerMonto = new JSpinner();
 		spinnerMonto.setModel(new SpinnerNumberModel(new Float(1), new Float(1), new Float(2147483647), new Float(1)));
-		spinnerMonto.setBounds(300, 30, 50, 20);
+		spinnerMonto.setBounds(300, 70, 50, 20);
 		getContentPane().add(spinnerMonto);
 		
 		btnAceptar = new JButton("Aceptar");
-		btnAceptar.setBounds(155, 160, 90, 23);
+		btnAceptar.setBounds(158, 207, 90, 23);
 		getContentPane().add(btnAceptar);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(110, 80, 220, 60);
+		scrollPane.setBounds(110, 110, 240, 60);
 		scrollPane.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
 		getContentPane().add(scrollPane);
 		
@@ -67,10 +70,27 @@ public class GastoFijoForm extends JDialog{
 		textDescripcion.setLineWrap(true);
 		scrollPane.setViewportView(textDescripcion);
 		
+		textVencimiento = new JTextField();
+		textVencimiento.setColumns(10);
+		textVencimiento.setBounds(110, 70, 40, 20);
+		getContentPane().add(textVencimiento);
+		
+		JLabel lblVto = new JLabel("Vencimiento:");
+		lblVto.setBounds(30, 70, 80, 20);
+		getContentPane().add(lblVto);
+		
+		JLabel lblMesVto = new JLabel("//" + new DateTime().now().toString("MM"));
+		lblMesVto.setBounds(158, 70, 80, 20);
+		getContentPane().add(lblMesVto);
+		
 	}
 
 	public JTextField getTextNombre() {
 		return textNombre;
+	}
+	
+	public JTextField getTextVencimento() {
+		return textVencimiento;
 	}
 	
 	public JTextArea getTextDescripcion() {
