33bf872a0397dc86035b7da4c875c96412a47e21
lucas <lucas.addisi@gmail.com>
2017-11-30 13:40:47 -0300

    Path changes in export.bat

--- export.bat
+++ export.bat
@@ -5,6 +5,6 @@ set password=%2
 set ip=%3
 set toSavePath=%4
 
-set sqlPath="C:\Program Files(x86)\MySQL\MySQL Server 5.7\bin\
+set sqlPath="C:\%programfiles(x86)%\MySQL\MySQL Server 5.5\bin\
 
 %sqlPath%mysqldump.exe" --user=%user% --password=%password% -h %ip% inmobiliaria -r %toSavePath%
